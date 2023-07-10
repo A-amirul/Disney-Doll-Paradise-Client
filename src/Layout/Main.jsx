a798a251aaef00450e22a9dc58d023a6ddad0849
A-amirul <amirulislamamir412@gmail.com>
2023-07-10 11:41:03 +0600

    bg color change

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -4,7 +4,7 @@ import Header from '../pages/Shared/Header';
 
 const Main = () => {
 	return (
-		<div>
+		<div className='bg-slate-900'>
 			<Header></Header>
 			<div className='min-h-[calc(100vh-340px)]'>
 				<Outlet></Outlet>
